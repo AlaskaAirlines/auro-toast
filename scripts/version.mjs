6bd30f40b692412726d59c73d54e2e15b117b30d
Jordan Jones <jmjones2463@gmail.com>
2024-07-09 15:26:28 -0700

    feat(version): implement custom versioned components

---
+++ scripts/version.mjs
@@ -0,0 +1,4 @@
+import versionWriter from '@aurodesignsystem/auro-library/scripts/build/versionWriter.js';
+
+versionWriter.writeDepVersionFile('@aurodesignsystem/auro-button');
+versionWriter.writeDepVersionFile('@aurodesignsystem/auro-icon');
