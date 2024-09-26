ec76cc76c3b386b0a13194a8ec8d68f6f03465f6
Jordan Jones <jmjones2463@gmail.com>
2024-09-26 13:10:10 -0700

    perf: refactor custom component registration config

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 import { AuroToast } from './src/auro-toast.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @returns {void}
- */
- const registerComponent = (name = 'custom-toast') => {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends AuroToast {});
-  }
-}
-
-export { registerComponent }
+RuntimeUtils.default.prototype.registerComponent('custom-toast', AuroToast);
