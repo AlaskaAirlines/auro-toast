a703a8b99a2e1753327b7816d0289e924742a8ce
Alexis Bolaños Avalos <alexis.avalos@alaskaair.com>
2025-09-26 11:51:31 -0600

    fix(toaster): fixed issue regarding scss file import and added register to toaster so users can call it directly

--- src/auro-toaster.js
+++ src/auro-toaster.js
@@ -5,12 +5,26 @@
 
 // If using litElement base class
 import { LitElement, html } from "lit";
-import styleCss from "./styles/style-toaster.scss";
+import styleCss from "./styles/style-toaster-css.js";
 
-export default class AuroToaster extends LitElement {
+import AuroLibraryRuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
+
+export class AuroToaster extends LitElement {
 
   static get styles() {
     return [styleCss];
+  }
+
+  /**
+   * This will register this element with the browser.
+   * @param {string} [name="auro-toaster"] - The name of element that you want to register to.
+   *
+   * @example
+   * AuroToaster.register("custom-toast") // this will register this element to <custom-toast/>
+   *
+   */
+  static register(name = "auro-toaster") {
+    AuroLibraryRuntimeUtils.prototype.registerComponent(name, AuroToaster);
   }
 
   render() {
